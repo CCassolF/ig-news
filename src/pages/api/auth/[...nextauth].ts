737bde9409012575741c0e170115d79af3879f3e
carlos <carloscassolfilho@gmail.com>
2023-05-10 16:22:41 -0300

    Salvando usuario no banco

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from "../../../services/fauna"
 
 export const authOptions = {
   providers: [
@@ -13,6 +17,23 @@ export const authOptions = {
       },
     }),
   ],
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            { data: { email } }
+          )
+        )
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 }
 
 export default NextAuth(authOptions)
